de429e045dc501f331c34d20a5ac49c62cb3d2f9
fikriazhr <159441642+fikriazhr@users.noreply.github.com>
2025-08-29 14:09:05 +0700

    Update diy-part1.sh
    delete unused repo

--- script/openwrt/diy-part1.sh
+++ script/openwrt/diy-part1.sh
@@ -10,11 +10,9 @@
 # sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 # sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
 
-# Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
-sed -i '$a src-git lynxnexy https://github.com/lynxnexy/packages' feeds.conf.default
+# Add a feed source
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git modemfeed https://github.com/koshev-msk/modemfeed' feeds.conf.default
